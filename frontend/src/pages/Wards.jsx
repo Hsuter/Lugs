13ada5e2f2442836a32b7f159a380ae5735d2c4e
Harry Suter <harry.suter.hs@gmail.com>
2023-06-01 14:52:40 +0300

    wards

--- frontend/src/pages/Wards.jsx
+++ frontend/src/pages/Wards.jsx
@@ -2,31 +2,40 @@ import React from "react";
 
 const Wards = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-8  my-8">
       <h1 className="text-green text-[30px] underline-motion ">
         Lugari Constituency Wards
       </h1>
-      <p> </p>
-      <div>
+
+      <div className="items-center flex flex-col mx-8 ">
         <h1 className="text-green text-[15px] font-bold">Mautuma ward</h1>
         <p>
           Mautuma County assembly ward is an electoral ward in Kenya. It is one
           of the wards in Lugari constituency in Kakamega county. . It has
           Registered voters per polling station by electoral area/ward and
-          constitutes of an approximate population of 25,082 . Community Areas
-          IVONA MAKONGENI SIPANDE MILIMANI SINOKO KEWA
+          constitutes of an approximate population of 25,082 .
+          <p>
+            Community Areas; <li>IVONA</li> <li> MAKONGENI</li> <li>SIPANDE</li>{" "}
+            <li>MILIMANI</li> <li>SINOKO</li> <li>KEWA</li>
+          </p>
         </p>
       </div>
-      <div>
-        <h1 className="text-green text-[15px] font-bold">Lugari ward</h1>
-      </div>
-      <div>
+
+      <div className="items-center flex flex-col mx-8 ">
         <h1 className="text-green text-[15px] font-bold">CHEKALINI ward</h1>
-        <p>
+        <p className="flex flex-col w-full">
           Chekalini Ward is one of the six wards within Kakamega county, Lugari
-          Constituency, Lugari Sub-county, it is devided into the following
-          community areas: ITUMBU MILIMANI KABURENGU MBAJO MUKHUYU s
+          Constituency.
+          <br /> Lugari Sub-county, it is devided into the following community
+          areas:<li>ITUMBU</li>
+          <li>MILIMANI</li>
+          <li>KABURENGU</li>
+          <li>MBAJO</li>
+          <li>MUKHUYU</li>
         </p>
+      </div>
+      <div className="flex flex-col items-center">
+        <h1 className="text-green text-[15px] font-bold">Lugari ward</h1>
       </div>
     </div>
   );
